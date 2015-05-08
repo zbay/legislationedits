bf9699cfc0592db8c736e7e9450364232b5623e3
zbay <zlwilliams1@gmail.com>
2015-05-08 18:34:17 -0400

    Gave retrieveEdits.py a main method

--- retrieveEdits/retrieveEdits.py
+++ retrieveEdits/retrieveEdits.py
@@ -4,51 +4,51 @@ import time
 from twython import Twython
 from creds import *
 
-twitter = Twython(APP_KEY, APP_SECRET,
-                  FINAL_OAUTH_TOKEN, FINAL_OAUTH_TOKEN_SECRET)
+def main():
+                  twitter = Twython(APP_KEY, APP_SECRET, FINAL_OAUTH_TOKEN, FINAL_OAUTH_TOKEN_SECRET)
 
-#NUMBER OF SECONDS BETWEEN CHECKS
-frequency = 600.0
+                  #NUMBER OF SECONDS BETWEEN CHECKS
+                  frequency = 600.0
 
-st = time.time()
-startTime = st - frequency
+                  st = time.time()
+                  startTime = st - frequency
 
-outputFile = open("whitelist114.txt", 'r')
+                  outputFile = open("whitelist114.txt", 'r')
 
-for line in outputFile:
-  line = line.replace (" ", "_")
-  line = line.replace("\n", "")
-  if len(line) > 1: #making sure an empty line isn't fed in to the url
-    url =  "http://en.wikipedia.org/w/api.php?action=query&rvdiffto=prev&prop=revisions&rvprop=timestamp|user&format=json&titles=" + line + "&redirects"
-    jsonurl = urllib2.urlopen(url)
-    revision = json.loads(jsonurl.read())
-    query = revision["query"]
-    pages = query["pages"]
-    page = next (iter (pages.values()))
-    revisions = page["revisions"]
-    step = revisions[0]
-    timestamp = step['timestamp']
-    user = step['user']
-    diff = step['diff']
-    fromID = str(diff['from'])
-    toID = str(diff['to'])
+                  for line in outputFile:
+                                    line = line.replace (" ", "_")
+                                    line = line.replace("\n", "")
+                                    if len(line) > 1: #making sure an empty line isn't fed in to the url
+                                                      url =  "http://en.wikipedia.org/w/api.php?action=query&rvdiffto=prev&prop=revisions&rvprop=timestamp|user&format=json&titles=" + line + "&redirects"
+                                                      jsonurl = urllib2.urlopen(url)
+                                                      revision = json.loads(jsonurl.read())
+                                                      query = revision["query"]
+                                                      pages = query["pages"]
+                                                      page = next (iter (pages.values()))
+                                                      revisions = page["revisions"]
+                                                      step = revisions[0]
+                                                      timestamp = step['timestamp']
+                                                      user = step['user']
+                                                      diff = step['diff']
+                                                      fromID = str(diff['from'])
+                                                      toID = str(diff['to'])
   
  # 2013-02-05T09:37:29Z
   
-  time1 = time.strptime(timestamp, "%Y" + "-" + "%m" + "-" + "%d" + "T" + "%H" + ":" + "%M" + ":" "%S" + "Z")
-  revisionTime = time.mktime(time1)
-  time2 = time.strftime("%H" + ":" + "%M" + " UTC", time1)
+                                                      time1 = time.strptime(timestamp, "%Y" + "-" + "%m" + "-" + "%d" + "T" + "%H" + ":" + "%M" + ":" "%S" + "Z")
+                                                      revisionTime = time.mktime(time1)
+                                                      time2 = time.strftime("%H" + ":" + "%M" + " UTC", time1)
   
-if revisionTime > startTime:
-    #if user[:8] == "143.231.": #House IP address. A regular expression version would be better but I'm lazy
-     # output = "ANONYMOUS US HOUSE EDIT: Article /" + line + "\""
-    #elif user[:8] == "156.33.": #Senate IP address. Regular expression ideal, again
-     # output = "ANONYMOUS SENATE EDIT: Article /" + line + "\""
-    #else:
-    lineReadable = line.replace ("_", " ")
-    lineReadable = lineReadable.replace("%E2%80%99", "'")
-    lineReadable = lineReadable.replace("%27", "'")
-    output = "Article \"" + lineReadable + "\" edited by " + user
+                                                      if revisionTime > startTime:
+                                                                        #if user[:8] == "143.231.": #House IP address. A regular expression version would be better but I'm lazy
+                                                                        # output = "ANONYMOUS US HOUSE EDIT: Article /" + line + "\""
+                                                                        #elif user[:8] == "156.33.": #Senate IP address. Regular expression ideal, again
+                                                                        #output = "ANONYMOUS SENATE EDIT: Article /" + line + "\""
+                                                                        #else:
+                                                                        lineReadable = line.replace ("_", " ")
+                                                                        lineReadable = lineReadable.replace("%E2%80%99", "'")
+                                                                        lineReadable = lineReadable.replace("%27", "'")
+                                                                        output = "Article \"" + lineReadable + "\" edited by " + user
      
-    edit = output[:116] + " http://en.wikipedia.org/w/index.php?title=" + line + "&diff=" + toID + "&oldid=" + fromID
-    twitter.update_status(status=edit)
+                                                                        edit = output[:116] + " http://en.wikipedia.org/w/index.php?title=" + line + "&diff=" + toID + "&oldid=" + fromID
+                                                                        twitter.update_status(status=edit)
